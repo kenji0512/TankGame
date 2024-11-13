4ede98dbce824ef2e64fed59118c0820471baec7
DESKTOP-9FM1A4D\vantan <mjoni3899@gmail.com>
2024-11-14 02:14:05 +0900

    スクリプトを修正

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,30 +6,37 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float lifetime = 5f; // 弾の寿命（秒）
     [SerializeField] public GameObject _hitEffectPrefab;//衝突エフェクトのプレハブ
     [SerializeField] public int damageAmount = 10;//ダメージ量
-    public PlayerType shooterType;
+    public PlayerType shooterType;// 発射者のプレイヤータイプ
 
     protected virtual void Start()
     {
         // 一定時間後に弾を自動で破壊
         Destroy(gameObject, lifetime);
     }
+    private Vector3 _direction;  // 弾の移動方向
 
+    public void SetDirection(Vector3 direction)
+    {
+        _direction = direction;
+    }
     protected virtual void Update()
     {
         // 弾を前方に移動させる
-        transform.position += transform.forward * speed * Time.deltaTime;
+        transform.position += _direction * speed * Time.deltaTime;
     }
 
     protected void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerController>() != null)
         {
-            var targetCharacter = other.GetComponent<Character>();
+            //var targetCharacter = other.GetComponent<Character>();
             PlayerController hitPlayer = other.GetComponent<PlayerController>();
+            Debug.Log($"hitPlayer.type : {hitPlayer._playerType}\nshooterType : {shooterType}");
+
             if (hitPlayer._playerType != shooterType)
             {
-                HandleCharacterCollision(targetCharacter);
-                Debug.Log($"Bullet hit {hitPlayer.gameObject.name} and dealt damage! Remaining Health: {targetCharacter.GetCurrentHealth()}");
+                HandleCharacterCollision(hitPlayer);
+                Debug.Log($"Bullet hit {hitPlayer.gameObject.name} and dealt damage! Remaining Health: {hitPlayer.GetCurrentHealth()}");
                 // 衝突した相手が発射者でない場合、処理を行う
                 Destroy(gameObject); // 弾を消去
             }
@@ -41,6 +48,7 @@ public class Bullet : MonoBehaviour
             {
                 HandleWallCollision(breakableWall);
             }
+            Debug.Log($"Bullet hit {breakableWall}");
         }
     }
     protected virtual void HandleWallCollision(BreakableWall breakableWall)
@@ -49,10 +57,10 @@ public class Bullet : MonoBehaviour
         CreateHitEffect(); // 衝突エフェクトを生成
         Destroy(gameObject); // 弾を破壊
     }
-    protected virtual void HandleCharacterCollision(Character targetCharacter)
+    protected virtual void HandleCharacterCollision(PlayerController hitPlayer)
     {
         // プレイヤーに当たった場合、ダメージを与える
-        targetCharacter.TakeDamage();
+        hitPlayer.TakeDamage(damageAmount);
         CreateHitEffect();
         Destroy(gameObject); // 弾を破壊
         Debug.Log("Destroy" + gameObject.name);
