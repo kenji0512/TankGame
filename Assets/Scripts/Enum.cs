b6a53a82fa59b8a5d9bc8fad590f63d615f93ca4
DESKTOP-9FM1A4D\vantan <mjoni3899@gmail.com>
2024-12-07 12:04:33 +0900

    リファクタリングをした

--- Assets/Scripts/Enum.cs
+++ Assets/Scripts/Enum.cs
@@ -2,13 +2,19 @@ public enum PlayerType
 {
     Player1, Player2, Player3, Player4, Player5, Player6,
 }
-public enum State
-{
-    Idle, moving, Shooting, Dead
-}
 public enum GameState
 {
     Playing,
     Paused,
     GameOver
 }
+[System.Flags]
+public enum State
+{
+    Idle = 0,
+    Moving = 1 << 0,
+    Shooting = 1 << 1,
+    Dead = 1 << 2
+}
+
+//ŽžŠÔ‚É‚Ü‚Â‚í‚éŒ^‚Ífloat‚¶‚á‚È‚­‚Ädoble‚É‚·‚é‚Ì‚ª‚¢‚¢
