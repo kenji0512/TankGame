ce8cf2dcd477880299034b3362a18c4fddaf036c
kenji0512 <kenji.joni@gmail.com>
2025-09-10 06:47:12 +0900

    BGMを追加とその他バグ修正

--- Assets/Scripts/HPBar.cs
+++ Assets/Scripts/HPBar.cs
@@ -5,18 +5,18 @@ using DG.Tweening;
 public class HPBar : MonoBehaviour
 {
     [Header("参照")]
-    [SerializeField] private Slider frontBar;   // 即時減るバー（赤）
-    [SerializeField] private Slider delayedBar; // 遅れて減るバー（黄など）
+    [SerializeField] private Slider _frontBar;   // 即時減るバー（赤）
+    [SerializeField] private Slider _delayedBar; // 遅れて減るバー（黄など）
 
     public void UpdateHP(float currentHP, float maxHP)
     {
         float targetValue = currentHP / maxHP;
 
         // 即時反映バー
-        frontBar.value = targetValue;
+        _frontBar.value = targetValue;
 
         // 遅れて減少するバー
-        delayedBar.DOValue(targetValue, 0.5f)
+        _delayedBar.DOValue(targetValue, 0.5f)
                   .SetEase(Ease.OutQuad);
     }
 }
